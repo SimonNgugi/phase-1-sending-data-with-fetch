f4228997953a2b1e0cce408b7c784e975c938e76
Simon Ngugi Ngigi <simonngugi88@gmail.com>
2022-08-31 13:07:13 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Add your code here
+
+const configurationObject = {
+    method: "POST",
+    headers: {
+        "content-Type": "application/json", 
+        Accept: "application/json",
+    },
+    body: JSON.stringify({
+        name: "",
+        email: "",
+    }),
+};
+fetch("http://localhost:3000/users", configurationObject)
+.then(function(response){
+    return response.json();
+})
+.then(function(Object){
+    console.log(Object);
+})
+.catch(function(error){
+    console.log(error.message);
+})
